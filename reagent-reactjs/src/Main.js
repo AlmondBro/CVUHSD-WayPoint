59c9f269980962865bd93e880545611e241d372c
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2018-03-23 14:04:01 -0700

    Added some components from our HTML. Could start the routing now, but would rather modularize the HTML or components some more.

---
+++ reagent-reactjs/src/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from "React";
+class Main extends Component {
+    render() {
+        return(
+            <main>
+                <header class="redToDarkRedgradient">
+                    <a href="#"><img src="https://portal.centinela.k12.ca.us/images/CV-600x600.png" class="img-responsive" id="reagentLogo" alt="Centinela Valley Union High School District Logo" /></a>
+                    <h1>Waypoint</h1>
+                </header>
+            </main>
+        );
+    }
+}
