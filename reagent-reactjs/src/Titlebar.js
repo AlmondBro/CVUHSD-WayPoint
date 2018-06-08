eae18e7a3ee40d44db809da7e8a061d807f095f7
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2018-06-08 13:56:24 -0700

    Added the titlebar functional or stateless component.

---
+++ reagent-reactjs/src/Titlebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const titlebar = () => {
+        return ( 
+        <div id="titlebar">
+            <div class="titleBarButtons-container">
+                <div class="titleBar-button" id="button-feedback" title="Provide Feedback">
+                    <img src="img/icon-feedback.png" alt="feedback" />
+                </div>
+
+                <div class="titleBar-button" id="button-minimize" title="Minimize Window">
+                    <img src="img/icon-minimize.png" alt="minimize" />
+                </div>
+
+                <div class="titleBar-button" id="button-close" title="Close Window">
+                    <img src="img/icon-close.png" alt="close" />
+                </div>
+            </div>
+
+            <div id="title">
+                <div id="wp-icon-container">
+                    <img id="wp-icon" src="img/wp-icon-grey.png" title="Waypoint Version 0.1" alt="WayPoint Icon" />
+                </div>
+                <h2 id="page-title">Home</h2>
+            </div>
+        </div>);
+};
+
+export default titlebar;
