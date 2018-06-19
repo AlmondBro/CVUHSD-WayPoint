f3fd20cacb11ba6ce93f3ff953972be1dd0a8b5b
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2018-06-19 15:29:06 -0700

    Got to load the full username in the console.

--- reagent-reactjs/src/Footer.js
+++ reagent-reactjs/src/Footer.js
@@ -6,17 +6,47 @@ const os = remote.require("os");
 const fs = electron.remote.require('fs');
 const ipcRenderer  = electron.ipcRenderer;
 
+const completeUserName = remote.require('fullname');
+
+completeUserName().then(name => {
+    console.log("Name:\t" + name);
+    getUserName(name);
+	//=> 'Sindre Sorhus'
+});
+
+const getUserName = (userName) => {
+    return userName;
+}
+
+const fullNameOfUser = getUserName();
+console.log("fullNameOfUser:\t" + fullNameOfUser);
+
 let networkInterfacesModule = os.networkInterfaces()["Local Area Connection"][1].address;
 console.log("networkInterfacesModule:\t" + networkInterfacesModule); 
 //let IP_Address = require("os").networkInterfaces["Local Area Connection"][1].address
 //console.log("IP Address:\t" + IP_Address);
 console.log("Networkdfdfdfddf Interface Obj:\t" + JSON.stringify(os.networkInterfaces()) );
 
+const determineWindowsVersion = (releaseNumber) => {
+    let windowsVersion;
+    let releaseNumberInt = parseInt(releaseNumber);
+    if (releaseNumberInt >= 6 && releaseNumberInt <= 8 ) {
+        windowsVersion = "Windows 7"
+    }
+
+    else  if (releaseNumberInt >= 8 ) {
+        windowsVersion = "Windows 10"
+    }
+
+    return windowsVersion;
+}
+
 const footer = (props) => {
     return (
         <footer>
-            <p>Welcome <span className="currentUserName">{props.userName || "Your UserName"}</span></p>
+            <p>Welcome <span className="currentUserName">{fullNameOfUser || "UserName"}</span></p>
             <p className="IP-message">Your IP Addresss:&#9;<span>{networkInterfacesModule || "Your IP Address"}</span></p>
+            <p className="OS-platform">Operating System:&#9;<span>{determineWindowsVersion(os.release()) || "OS Platform"}</span></p>
             <p className="cv-way">Powered by: The CV-Way</p>
         </footer>
     );
