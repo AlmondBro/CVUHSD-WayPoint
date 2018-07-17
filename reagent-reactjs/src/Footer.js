33567e1c5af7d5771e74bd538cfd25c59db55001
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2018-07-17 07:55:10 -0700

    Getting full username is now working.

--- reagent-reactjs/src/Footer.js
+++ reagent-reactjs/src/Footer.js
@@ -1,63 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
+
 const electron = window.require("electron");
 const remote = window.require("electron").remote;
 const os = remote.require("os");
 
-const fs = electron.remote.require('fs');
-const ipcRenderer  = electron.ipcRenderer;
-
 const completeUserName = remote.require('fullname');
-const macaddress = remote.require('macaddress');
+let macaddress = remote.require('macaddress');
 
-var yolo = this;
-
-completeUserName().then(name => {
-    console.log("Name:\t" + name);
-    getUserName(name);
-    yolo = name;
-	//=> 'Sindre Sorhus'
-});
-
-const getUserName = (userName) => {
-    return userName;
-}
-
-console.log("MacAddress:\t" + JSON.stringify(macaddress.networkInterfaces(), null, 2));
-console.log("IPv4:\t" + macaddress.networkInterfaces()["Local Area Connection"]["ipv4"]);
-
-var fullNameOfUser = getUserName();
-console.log("fullNameOfUser:\t" + fullNameOfUser);
-
-let IP_Address =  macaddress.networkInterfaces()["Local Area Connection"]["ipv4"] || macaddress.networkInterfaces()["Wi-Fi"]["ipv4"];
-//os.networkInterfaces()["Local Area Connection"][1].address || os.networkInterfaces()["Wi-Fi"][1].address;
-console.log("IP_Address:\t" + IP_Address); 
-//let IP_Address = require("os").networkInterfaces["Local Area Connection"][1].address
-//console.log("IP Address:\t" + IP_Address);
-console.log("Network Interface Obj:\t" + JSON.stringify(os.networkInterfaces()) );
-
-const determineWindowsVersion = (releaseNumber) => {
-    let windowsVersion;
-    let releaseNumberInt = parseInt(releaseNumber);
-    if (releaseNumberInt >= 6 && releaseNumberInt <= 8 ) {
-        windowsVersion = "Windows 7"
+class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: os.userInfo().username
+        };
+        
+        this.IP_Address = macaddress.networkInterfaces()["Local Area Connection"]["ipv4"] || macaddress.networkInterfaces()["Wi-Fi"]["ipv4"] || os.networkInterfaces()["Local Area Connection"][1].address || os.networkInterfaces()["Wi-Fi"][1].address || os.networkInterfaces()["Local Area Connection"][1].address || os.networkInterfaces()["Wi-Fi"][1].address;
     }
 
-    else  if (releaseNumberInt >= 8 ) {
-        windowsVersion = "Windows 10"
+    componentDidMount = () => { 
+        console.log("OS Network Interface Obj:\t" + JSON.stringify(os.networkInterfaces()) );
+        console.log("MacAddress:\t" + JSON.stringify(macaddress.networkInterfaces(), null, 2));
+        console.log("OS username:\t" + os.userInfo().username);
+
+        completeUserName().then(name => {
+            console.log("Complete userName:\t" + name);
+            this.setState({
+                userName: name
+            });
+            //=> 'Sindre Sorhus'
+        });
     }
 
-    return windowsVersion;
+    determineWindowsVersion = (releaseNumber) => {
+        let windowsVersion;
+        let releaseNumberInt = parseInt(releaseNumber);
+        if (releaseNumberInt >= 6 && releaseNumberInt <= 8 ) {
+            windowsVersion = "Windows 7"
+        }
+    
+        else  if (releaseNumberInt >= 8 ) {
+            windowsVersion = "Windows 10"
+        }
+    
+        return windowsVersion;
+    }
+    
+    render() {
+        return (
+            <footer>
+                <div className="USER-container"><p>User: <span className="currentUserName">{ this.state.userName || "Your Username" }</span></p></div>
+                <div className="IP-container"><p className="IP-message">IP Addresss:&#9;<span>{this.IP_Address || "Your IP Address"}</span></p></div>
+                <div className="OS-container"><p className="OS-platform">System:&#9;<span>{this.determineWindowsVersion(os.release()) || "OS Platform"}</span></p></div>
+                <p className="cv-way">Powered by: The CV-Way</p>
+            </footer>
+        );
+    }
 }
 
-const footer = (props) => {
-    return (
-        <footer>
-            <div className="USER-container"><p>User: <span className="currentUserName">{fullNameOfUser || "UserName"}</span></p></div>
-            <div className="IP-container"><p className="IP-message">IP Addresss:&#9;<span>{IP_Address || "Your IP Address"}</span></p></div>
-            <div className="OS-container"><p className="OS-platform">System:&#9;<span>{determineWindowsVersion(os.release()) || "OS Platform"}</span></p></div>
-            <p className="cv-way">Powered by: The CV-Way</p>
-        </footer>
-    );
-};
-
-export default footer;
+export default Footer;
