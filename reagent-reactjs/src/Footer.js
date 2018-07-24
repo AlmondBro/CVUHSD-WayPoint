8899f325ea12c82f9070d42fec072670fa6a873a
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2018-07-24 06:34:47 -0700

    Try to fix IP address detection.

--- reagent-reactjs/src/Footer.js
+++ reagent-reactjs/src/Footer.js
@@ -6,6 +6,7 @@ const os = remote.require("os");
 
 const completeUserName = remote.require("fullname");
 const macaddress = remote.require("macaddress");
+const undefsafe = remote.require("undefsafe");
 
 class Footer extends Component {
     constructor(props) {
@@ -15,9 +16,28 @@ class Footer extends Component {
             ipAddress: this.IP_Address
         };
         
-       // this.IP_Address = os.networkInterfaces()["Ethernet"][1].address || os.networkInterfaces()["Wi-Fi"][1].address;
-        this.IP_Address = macaddress.networkInterfaces()["Local Area Connection"]["ipv4"] || macaddress.networkInterfaces()["Wi-Fi"]["ipv4"] || os.networkInterfaces()["Local Area Connection"][1].address || os.networkInterfaces()["Wi-Fi"][1].address || os.networkInterfaces()["Local Area Connection"][1].address || os.networkInterfaces()["Wi-Fi"][1].address;
-        console.log("IP_Address:\t" + this.IP_Address); 
+       this.IP_Address = this.getIPAddress();
+       console.log("IP_Address:\t" + this.IP_Address); 
+    }
+
+    getIPAddress = () => {
+        let IP_Address; 
+        if ( undefsafe(macaddress, '"Local Area Connection"."ipv4"') !== undefined) {
+          //  IP_Address = macaddress.networkInterfaces()["Local Area Connection"]["ipv4"];
+          IP_Address = undefsafe(macaddress, '"Local Area Connection"."ipv4"');
+        }
+
+        else if ( undefsafe(macaddress, '"Wi-Fi"."ipv4"') !== "undefined") {
+            IP_Address =  macaddress.networkInterfaces()["Wi-Fi"]["ipv4"]; 
+            console.log("IPV4 wifi:\t" + IP_Address);
+            console.log("Typeof:\t"+ typeof IP_Address);
+        }
+/*
+        else {
+            IP_Address = "127.0.0.1";
+        } */
+
+        return IP_Address;
     }
 
     componentDidMount = () => { 
@@ -38,10 +58,21 @@ class Footer extends Component {
     determineWindowsVersion = (releaseNumber) => {
         let windowsVersion;
         let releaseNumberInt = parseInt(releaseNumber, 10);
+        console.log(releaseNumber);
+        console.log(releaseNumberInt);
+
+        if (releaseNumber === 6) {
+            windowsVersion = "Windows 8"
+        }
+
         if (releaseNumberInt >= 6 && releaseNumberInt <= 8 ) {
             windowsVersion = "Windows 7"
         }
     
+        else if (releaseNumberInt >= 8 ) {
+            windowsVersion = "Windows 10"
+        }
+
         else if (releaseNumberInt >= 8 ) {
             windowsVersion = "Windows 10"
         }
@@ -53,7 +84,7 @@ class Footer extends Component {
         return (
             <footer>
                 <div className="USER-container"><p>User: <span className="currentUserName">{ this.state.userName || "Your Username" }</span></p></div>
-                <div className="IP-container"><p className="IP-message">IP Address:&#9;<span>{this.IP_Address || "Your IP Address"}</span></p></div>
+                <div className="IP-container"><p className="IP-message">IP Address:&#9;<span>{this.state.ipAddress || "Your IP Address"}</span></p></div>
                 <div className="OS-container"><p className="OS-platform">System:&#9;<span>{this.determineWindowsVersion(os.release()) || "OS Platform"}</span></p></div>
                 <p className="cv-way">Powered by: The CV-Way</p>
             </footer>
