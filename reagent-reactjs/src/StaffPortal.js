e0c777ce2b8e400c2a0b3f6ffe368c93f1cafb47
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2018-08-02 12:01:06 -0700

    Use classname instead of class to avoid the React warning in staffportal.js.

--- reagent-reactjs/src/StaffPortal.js
+++ reagent-reactjs/src/StaffPortal.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const staffPortal = () => {
     return (
-        <embed src="https://portal.centinela.k12.ca.us/staff.html" class="staff-portal-embed" />
+        <embed src="https://portal.centinela.k12.ca.us/staff.html" className="staff-portal-embed" />
     );
 };
 
