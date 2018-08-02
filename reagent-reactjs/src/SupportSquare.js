861d49edd07691dfefa44d7582b2931c45d859cc
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2018-08-02 11:30:25 -0700

    Conditionally render NavLink components only if there is a navlink prop passed.

--- reagent-reactjs/src/SupportSquare.js
+++ reagent-reactjs/src/SupportSquare.js
@@ -3,14 +3,24 @@ import {NavLink} from "react-router-dom";
 import PropTypes from "prop-types";
 
 const SupportSquare = (props) => {
-    return(
-        <section className="support-square" id={props.id}>
-                <NavLink to={props.pageLink || props.phone}  >
+    if (props.pageLink) {
+        return(
+            <section className="support-square" id={props.id}>
+                    <NavLink to={props.pageLink}>
+                        <img src={props.imgSrc} className="square-icon" alt="Square-Icon" />
+                        <h3>{props.title}</h3>
+                    </NavLink>
+            </section>); 
+    } else {
+        return(
+            <section className="support-square" id={props.id}>
+                <a href={props.phone}>
                     <img src={props.imgSrc} className="square-icon" alt="Square-Icon" />
                     <h3>{props.title}</h3>
-                </NavLink>
+                </a>
             </section>);
-};
+    } //end else-statement
+}; //end SupportSquare functional component
 
 SupportSquare.propTypes = {
     pageLink: PropTypes.string,
