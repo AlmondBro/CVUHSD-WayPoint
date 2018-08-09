e4d8308f40ff38fb64011bdc4c068938c4572b35
Lopez <lopezj@centinela.k12.ca.us>
2018-08-09 08:03:40 -0700

    Add conditional in Staff Portal component to check if the portal webview element exists.

--- reagent-reactjs/src/StaffPortal.js
+++ reagent-reactjs/src/StaffPortal.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 window.onload = function() {
-    document.getElementById("portal-webview").addEventListener("new-window", (e) => {
-        const {BrowserWindow} = window.require('electron').remote;
-        const protocol = require("url").parse(e.url).protocol;
-        if (protocol === "http:" || protocol === "https:") {
-            const {shell} = window.require("electron");
-            shell.openExternal(e.url)
-            /* //Load in a new electron window
-          //let win = new BrowserWindow({width: 800, height: 600})
-          //win.loadURL(e.url); */
-        } //end if-statement
-      }); //end addEventListener();
-} //end if-statement
+    if (document.body.contains( document.getElementById("portal-webview") )) {
+        document.getElementById("portal-webview").addEventListener("new-window", (e) => {
+            const {BrowserWindow} = window.require('electron').remote;
+            const protocol = require("url").parse(e.url).protocol;
+            if (protocol === "http:" || protocol === "https:") {
+                const {shell} = window.require("electron");
+                shell.openExternal(e.url)
+                /* //Load in a new electron window
+              //let win = new BrowserWindow({width: 800, height: 600})
+              //win.loadURL(e.url); */
+            } //end if-statement
+          }); //end addEventListener()
+    } //end if-statement
+
+} //end window.onload
 
 const staffPortal = () => {
     return (
