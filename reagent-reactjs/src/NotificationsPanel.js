30b978698f5d34ce920b92a35dca35d7d9b72ecc
Juan David Lopez <juandavidlopez95@yahoo.com>
2018-08-22 08:46:12 -0700

    Remove Notification component copies in NotificationsPAnel.js

--- reagent-reactjs/src/NotificationsPanel.js
+++ reagent-reactjs/src/NotificationsPanel.js
@@ -5,26 +5,20 @@ import Notification from "./Notification.js";
 
 const notificationsPanel = (props) => {
     return (
-    <section className="notifications-section">
-        <h4 className="inline">Notifications</h4>
-        <button className="inline redToDarkRedgradient clickable" id="clear-button">Clear</button>
-        <div className="notifications-content">
-            <Notification urgent={true} notificationText="Update Required" />
+        <section className="notifications-section">
+            <h4 className="inline">Notifications</h4>
+            <button className="inline redToDarkRedgradient clickable" id="clear-button">Clear</button>
+            <div className="notifications-content">
+                <Notification urgent={true} notificationText="Update Required" />
 
-            <Notification urgent={false} notificationText="No HelpDesk Requests" faIconClassName="fa fa-check greenCheck" />
+                <Notification urgent={false} notificationText="No HelpDesk Requests" faIconClassName="fa fa-check greenCheck" />
 
-            <Notification urgent={false} notificationText="No HelpDesk Requests" faIconClassName="fa fa-check greenCheck" />
+                <Notification urgent={false} notificationText="10 files" faIconClassName="fas fa-archive archive" />
 
-            <Notification urgent={false} notificationText="No HelpDesk Requests" faIconClassName="fa fa-check greenCheck" />
-
-            <Notification urgent={false} notificationText="10 files" faIconClassName="fas fa-archive archive" />
-
-            <Notification urgent={false} notificationText="10 files" faIconClassName="fas fa-archive archive" />
-
-            <Notification urgent={false} notificationText="Coffee Taken" faIconClassName="fas fa-coffee coffee" />
-        </div>
-        <div className="blur-effect" id="notifications-blur-effect"></div>
-    </section>
+                <Notification urgent={false} notificationText="Coffee Taken" faIconClassName="fas fa-coffee coffee" />
+            </div>
+            <div className="blur-effect" id="notifications-blur-effect"></div>
+        </section>
     );
 };
 
