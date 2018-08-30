b3b83733c45ebd95ba0fe98d3db5550a10955463
Juan David Lopez <juandavidlopez95@yahoo.com>
2018-08-30 10:59:46 -0700

    Change SubmitTick to be a stateful component.

--- reagent-reactjs/src/SubmitTicket.js
+++ reagent-reactjs/src/SubmitTicket.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 //Import external components - ES6 JavaScript
 import Email from "./Email.js";
@@ -12,106 +12,39 @@ import FormButton from "./form-components/FormButton.js";
 
 //Import 3rd-party libraries
 import jsxToString from "jsx-to-string";
-import lifecycle from "react-pure-lifecycle";
 
-var pageTitle = "Message HelpDesk Support";
-
-let title;
-let description;
+class SubmitTicket extends Component {     
+    title;
+    description;
 //const clientName = document.getElementById("client-name").value;
-let email;
-let category;
-let location;
-let phoneExtension;
-let officeNumber;
+    email;
+    category;
+    location;
+    phoneExtension;
+    officeNumber;
 
-let fileAttachmentPath;
-let fileAttachmentName;
+    fileAttachmentPath;
+    fileAttachmentName;
 
-const fileAttachment = () => {
-    let file_input = document.getElementById("file-input");
-    let fileUpload_inputField = document.getElementById("uploadFile-path");
-    
-    const getFilePath = (file_input, fileUpload_inputField) => {
-        console.log("getFilePath()");
-        let fileUpload_valueArray = file_input.value.split("\\");
-        console.log("fileUpload_valueArray:\t", fileUpload_valueArray);
-        fileUpload_inputField.value =  fileUpload_valueArray[fileUpload_valueArray.length - 1];
-        fileAttachmentName = fileUpload_valueArray[fileUpload_valueArray.length - 1];
-        console.log("fileAttachmentName:\t" + fileAttachmentName);
+    constructor(props) {
+        super(props);
+        var pageTitle = "Message HelpDesk Support";
     }
-    //Do not allow dragging over
-    document.addEventListener("dragover",  (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-    });
 
-    const formGetPathCode = () => {
-        file_input.addEventListener("change", (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-
-            console.log("onchange");
-            getFilePath(file_input, fileUpload_inputField);
-            console.log("transfer:\t" + e.dataTransfer);
-            console.log("path:\t" + file_input.files[0].path);
-            document.getElementById("uploadFile-path").value = fileAttachmentName ;
-
-            fileAttachmentPath = file_input.files[0].path;
-            console.log("FileattachmentPath:\t" + fileAttachmentPath );
-           /* for (let f of e.dataTransfer.files) {
-                console.log('File(s) you dragged here: ', f.path)
-            } //end for loop */
-
-            // this.value = null; 
-            //return false; 
-        });
-    } //end formGetPathCode()
-
-  if (file_input != null) {
-        formGetPathCode();
-    }   
-};
-
- // Create your lifecycle methods for "react-pure-lifecycle"
-const componentDidMount = (props) => {
-    props.updateTitle(pageTitle);
-    props.renderFooter(false);
-    window.onload = fileAttachment();
-}
-
-// make the lifecycle methods properties on a standard object
-const methods = {
-    componentDidMount
-};
-
-const SubmitTicket = (props) => {
-    
-    window.onload = () => {
-        title = document.getElementById("summary");
-        description = document.getElementById("detailed-description");
-        // clientName = document.getElementById("client-name").value;
-        email = document.getElementById("client-email");
-        category = document.getElementById("category");
-        location = document.getElementById("location");
-        phoneExtension = document.getElementById("phone-extension");
-        officeNumber = document.getElementById("building-number");
-    } //end window.onload
-
-    const sendEmail = (e) => {
+    sendEmail = (e) => {
         ((e) => {
             e.preventDefault();
             console.log("preventSubmit");
              //return false;
         })(e); 
 
-        if (title.value === null || title.value  === undefined || 
-            description.value === null || description.value === undefined ||
-            email.value === null || email.value === undefined || 
-            category.value === null ||  category.value  === undefined || 
-            location.value === null || location.value === undefined || 
-            phoneExtension.value === null || phoneExtension.value  === undefined || 
-            officeNumber.value === null || officeNumber.value === undefined  ) {
+        if (this.title.value === null || this.title.value  === undefined || 
+            this.description.value === null || this.description.value === undefined ||
+            this.email.value === null || this.email.value === undefined || 
+            this.category.value === null ||  this.category.value  === undefined || 
+            this.location.value === null || this.location.value === undefined || 
+            this.phoneExtension.value === null || this.phoneExtension.value  === undefined || 
+            this.officeNumber.value === null || this.officeNumber.value === undefined  ) {
                 console.log("Undefined/null fields!");
                 return;
         } else {
@@ -130,25 +63,25 @@ const SubmitTicket = (props) => {
                                                  phoneExtension={phoneExtension}
                                                  officeNumber={officeNumber} />
                                         ); */
-                                        jsxToString(<Email  title={title} 
-                                                            description={description}
-                                                            email={email}
-                                                            category={category}
-                                                            location={location}
-                                                            phoneExtension={phoneExtension}
-                                                            officeNumber={officeNumber} />).toString();
+                                        jsxToString(<Email  title={this.title} 
+                                                            description={this.description}
+                                                            email={this.email}
+                                                            category={this.category}
+                                                            location={this.location}
+                                                            phoneExtension={this.phoneExtension}
+                                                            officeNumber={this.officeNumber} />).toString();
     
             console.log("Sent."); 
             console.log("HTMLMessage:\t" + HTMLmessage);
          
            sendmail({
-                from: email.value,
+                from: this.email.value,
                 to: "juandavidlopez95@yahoo.com",
-                subject: title.value,
+                subject: this.title.value,
                 html: HTMLmessage,
                 attachments: [  {   // file on disk as an attachment
-                                    filename: fileAttachmentName,
-                                    path: fileAttachmentPath // stream this file
+                                    filename: this.fileAttachmentName,
+                                    path: this.fileAttachmentPath // stream this file
                                 }
                             ]
               }, (err, reply) => {
@@ -157,71 +90,135 @@ const SubmitTicket = (props) => {
                 console.dir(reply);
            });  
         } //end else-statement
-    } //end sendMail() method
+    }; //end sendMail() method
 
-    return (
-        <form className="helpDeskTicket-form" action="https://helpdesk.centinela.k12.ca.us/portal/new_ticket" method="POST" encType="multipart/form-data">
-            <fieldset>
-                <legend className="form-legend">
-                    <h3>Message IT HelpDesk</h3>
-                </legend>
-                <p className="submitForm-inputContainer">
-                    <SingleInput label={true} labelTitle="Summary/Title" inputType="text" id="summary" placeholder="Title or summary of the technical issue..." onChange={()=>{}} />
-                </p>
-                <p className="submitForm-inputContainer">
-                    <TextArea label={true} labelTitle="Detailed Description" id="detailed-description" cols={5} rows={3}  placeholder="Type the technical issue you are facing here..." resize="vertical"/>
-                </p>
-                <p className="submitForm-inputContainer">
-                    <SingleInput label={true} labelTitle="Centinela E-mail" inputType="email" id="client-email" placeholder="Your Centinela e-mail..." />
-                </p>
-                <p className="inline fieldMargin">
-                    <Select 
-                        id="category" 
-                        label={true} 
-                        labelTitle="Category" 
-                        labelClassName="block" 
-                        options={["Computer Issue", "Printer Issue", "Projector Issue", "Password Issue", "Other Type of Issue"]} 
-                        placeholder="Problem Categories" />
-                </p>
-                <p className="inline fieldMargin">
-                    <Select id="location" 
+    fileAttachment = () => {
+        let file_input = document.getElementById("file-input");
+        let fileUpload_inputField = document.getElementById("uploadFile-path");
+        
+        const getFilePath = (file_input, fileUpload_inputField) => {
+            console.log("getFilePath()");
+            let fileUpload_valueArray = file_input.value.split("\\");
+            console.log("fileUpload_valueArray:\t", fileUpload_valueArray);
+            fileUpload_inputField.value =  fileUpload_valueArray[fileUpload_valueArray.length - 1];
+            this.fileAttachmentName = fileUpload_valueArray[fileUpload_valueArray.length - 1];
+            console.log("fileAttachmentName:\t" + this.fileAttachmentName);
+        }
+        //Do not allow dragging over
+        document.addEventListener("dragover",  (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+    
+        const formGetPathCode = () => {
+            file_input.addEventListener("change", (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+    
+                console.log("onchange");
+                getFilePath(file_input, fileUpload_inputField);
+                console.log("transfer:\t" + e.dataTransfer);
+                console.log("path:\t" + file_input.files[0].path);
+                document.getElementById("uploadFile-path").value = this.fileAttachmentName ;
+    
+                this.fileAttachmentPath = file_input.files[0].path;
+                console.log("FileattachmentPath:\t" + this.fileAttachmentPath );
+               /* for (let f of e.dataTransfer.files) {
+                    console.log('File(s) you dragged here: ', f.path)
+                } //end for loop */
+    
+                // this.value = null; 
+                //return false; 
+            });
+        } //end formGetPathCode()
+    
+      if (file_input != null) {
+            formGetPathCode();
+        } //end if-statement   
+    };
+
+    componentDidMount = (props) => {
+        this.props.updateTitle(this.pageTitle);
+        this.props.renderFooter(false);
+        window.onload = () => {
+            this.title = document.getElementById("summary");
+            this.description = document.getElementById("detailed-description");
+            // clientName = document.getElementById("client-name").value;
+            this.email = document.getElementById("client-email");
+            this.category = document.getElementById("category");
+            this.location = document.getElementById("location");
+            this.phoneExtension = document.getElementById("phone-extension");
+            this.officeNumber = document.getElementById("building-number");
+        } //end window.onload
+        window.onload = this.fileAttachment();
+    } //end componentDidMount()
+    
+    render = () => {
+        return (
+            <form className="helpDeskTicket-form" action="https://helpdesk.centinela.k12.ca.us/portal/new_ticket" method="POST" encType="multipart/form-data">
+                <fieldset>
+                    <legend className="form-legend">
+                        <h3>Message IT HelpDesk</h3>
+                    </legend>
+                    <p className="submitForm-inputContainer">
+                        <SingleInput label={true} labelTitle="Summary/Title" inputType="text" id="summary" placeholder="Title or summary of the technical issue..." onChange={()=>{}} />
+                    </p>
+                    <p className="submitForm-inputContainer">
+                        <TextArea label={true} labelTitle="Detailed Description" id="detailed-description" cols={5} rows={3}  placeholder="Type the technical issue you are facing here..." resize="vertical"/>
+                    </p>
+                    <p className="submitForm-inputContainer">
+                        <SingleInput label={true} labelTitle="Centinela E-mail" inputType="email" id="client-email" placeholder="Your Centinela e-mail..." />
+                    </p>
+                    <p className="inline fieldMargin">
+                        <Select 
+                            id="category" 
                             label={true} 
-                            labelTitle="Location" 
+                            labelTitle="Category" 
                             labelClassName="block" 
-                            options={["Lawndale", "Leuzinger", "Hawthorne", "Lloyde", "District Office"]} 
-                            placeholder="School Sites" />
-                </p>
-                <p className="inline fieldMargin">
-                    <SingleInput label={true} labelClassName="block" labelTitle="Phone Extension:" inputType="tel" id="phone-extension" placeholder="7811" />
-                </p>
-                <p className="inline fieldMargin">
-                    <SingleInput label={true} labelClassName="block" labelTitle="Office/Number #" inputType="text" id="building-number" placeholder="A13" />
-                </p>
-                <p>
-                    <label>Optional Attachment:</label>
-               
-                    <label className="fileUpload-button redToDarkRedgradient clickable" htmlFor="file-input">
-                            Upload File
-                    </label>
-                    
-                    <input type="file" name="attachment" id="file-input" value="" />
-
-                    <SingleInput label={true} labelClassName="" labelTitle="File name:" inputType="text" id="uploadFile-path" placeholder="File attachment name..." readOnly={true} />
-
-                   {/* 
-                        <label htmlFor="uploadFile-path">File name:</label>
-                        <input type="text" placeholder="Optional file path..." readOnly name="uploadFile-path" id="uploadFile-path" />
-                    */} 
+                            options={["Computer Issue", "Printer Issue", "Projector Issue", "Password Issue", "Other Type of Issue"]} 
+                            placeholder="Problem Categories" />
+                    </p>
+                    <p className="inline fieldMargin">
+                        <Select id="location" 
+                                label={true} 
+                                labelTitle="Location" 
+                                labelClassName="block" 
+                                options={["Lawndale", "Leuzinger", "Hawthorne", "Lloyde", "District Office"]} 
+                                placeholder="School Sites" />
+                    </p>
+                    <p className="inline fieldMargin">
+                        <SingleInput label={true} labelClassName="block" labelTitle="Phone Extension:" inputType="tel" id="phone-extension" placeholder="7811" />
+                    </p>
+                    <p className="inline fieldMargin">
+                        <SingleInput label={true} labelClassName="block" labelTitle="Office/Number #" inputType="text" id="building-number" placeholder="A13" />
+                    </p>
+                    <p>
+                        <label>Optional Attachment:</label>
                    
-                </p>
-                <p>
-                    <FormButton inputType="submit" className="redToDarkRedgradient clickable" buttonTitle="Submit" eventFunc={sendEmail}  />
-                    <FormButton inputType="reset" className="redToDarkRedgradient clickable" buttonTitle="Reset" />
-                </p>
-            </fieldset>
-    </form>
-    ); //end return statement
-}; //end submitTicket class
+                        <label className="fileUpload-button redToDarkRedgradient clickable" htmlFor="file-input">
+                                Upload File
+                        </label>
+                        
+                        <input type="file" name="attachment" id="file-input" value="" />
+    
+                        <SingleInput label={true} labelClassName="" labelTitle="File name:" inputType="text" id="uploadFile-path" placeholder="File attachment name..." readOnly={true} />
+    
+                       {/* 
+                            <label htmlFor="uploadFile-path">File name:</label>
+                            <input type="text" placeholder="Optional file path..." readOnly name="uploadFile-path" id="uploadFile-path" />
+                        */} 
+                       
+                    </p>
+                    <p>
+                        <FormButton inputType="submit" className="redToDarkRedgradient clickable" buttonTitle="Submit" eventFunc={this.sendEmail}  />
+                        <FormButton inputType="reset" className="redToDarkRedgradient clickable" buttonTitle="Reset" />
+                    </p>
+                </fieldset>
+        </form>
+        ); //end return statement
+    } //end render()
+   
+} //end SubmitTicket class
 
 //Decorate exported component with lifecycle methods
-export default lifecycle(methods)(SubmitTicket);
+export default SubmitTicket;
