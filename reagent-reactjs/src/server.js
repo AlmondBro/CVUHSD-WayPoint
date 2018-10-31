0f22ed8af20407574555d8041a139d956426aca3
JuanDavidLopez95 <juandavidlopez95@yahoo.com>
2018-10-31 13:31:06 -0700

    Transform single quotes to double quotes inside the corsanywhere() function in server.js

--- reagent-reactjs/src/server.js
+++ reagent-reactjs/src/server.js
@@ -1,17 +1,17 @@
 let corsAnywhere = () => {
   console.log("CORS Anywhere()");
   // Listen on a specific host via the HOST environment variable
-  var host = process.env.HOST || '0.0.0.0';
+  var host = process.env.HOST || "0.0.0.0";
   // Listen on a specific port via the PORT environment variable
   var port = process.env.PORT || 4000;
 
-  var cors_proxy = window.require('cors-anywhere');
+  var cors_proxy = window.require("cors-anywhere");
   cors_proxy.createServer({
       originWhitelist: [], // Allow all origins
-      requireHeader: ['origin', 'x-requested-with'],
-      removeHeaders: ['cookie', 'cookie2']
-  }).listen(port, host, function() {
-      console.log('Running CORS Anywhere on ' + host + ':' + port);
+      requireHeader: ["origin", "x-requested-with"],
+      removeHeaders: ["cookie", "cookie2"]
+  }).listen(port, host, () => {
+      console.log("Running CORS Anywhere on " + host + ":" + port);
   });
 }; //corsAnywhere()
 
