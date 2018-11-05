86276f48eb1b0d591454e952e5a9a43495ed8125
JuanDavidLopez95 <juandavidlopez95@yahoo.com>
2018-11-05 07:50:54 -0800

    Remove dragging of icons in the titlebar.

--- reagent-reactjs/src/Titlebar.js
+++ reagent-reactjs/src/Titlebar.js
@@ -4,21 +4,21 @@ const Titlebar = (props) => {
     return ( 
         <div id="titlebar" className="noHighlight noDrag">
             <div className="titleBarButtons-container">
-                <div className="titleBar-button" id="button-feedback" title="Provide Feedback">
-                    <img src="img/icon-feedback.png" alt="feedback" />
+                <div className="titleBar-button noDrag" id="button-feedback" title="Provide Feedback">
+                    <img src="img/icon-feedback.png" className="noDrag" alt="feedback" />
                 </div>
 
-                <div className="titleBar-button" id="button-minimize" title="Minimize Window">
-                    <img src="img/icon-minimize.png" alt="minimize" />
+                <div className="titleBar-button noDrag" id="button-minimize" title="Minimize Window">
+                    <img src="img/icon-minimize.png" className="noDrag" alt="minimize" />
                 </div>
 
-                <div className="titleBar-button" id="button-close" title="Close Window">
-                    <img src="img/icon-close.png" alt="close" />
+                <div className="titleBar-button noDrag" id="button-close" title="Close Window">
+                    <img src="img/icon-close.png" className="noDrag" alt="close" />
                 </div>
             </div>
 
             <div id="title">
-                <div id="wp-icon-container">
+                <div id="wp-icon-container" className="noDrag">
                     <img id="wp-icon" src="img/wp-icon-grey.png" title="Waypoint Version 0.1" alt="WayPoint Icon" />
                 </div>
                 <div id="page-title-container"><h2 id="page-title">{props.pageTitle}</h2></div>
