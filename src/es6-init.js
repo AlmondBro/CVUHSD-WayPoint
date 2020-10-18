131bdde1f2b7177994dfd3fa0b313ceda3ca029e
Juan David Londoño <juandavid@jdlondono.com>
2020-10-17 17:06:36 -0700

    Use ESM to use ES6 JavaScript code in the electron main process.

--- src/es6-init.js
+++ src/es6-init.js
@@ -1,5 +1,7 @@
-//To use ES6 code (like import) in the electron main process, compile it first with electron-compile.
+//To use ES6 code (like import) in the electron main process, compile it first with esm.
 var path = require("path");
 var appRoot = path.join(__dirname, '..');
 
-require('electron-compile').init(appRoot, require.resolve('./electron-starter.js'));
+// Set options as a parameter, environment variable, or rc file.
+require = require("esm")(module/*, options*/);
+module.exports = require('./electron-starter.js');
